bbcfcb117245c9f6de0cbe8dea111014c9547f3b
drrqueiroz <drrqueiroz@gmail.com>
2024-03-05 12:59:41 -0300

    Cadastrar Usuário - entidade usuário

---
+++ src/app/models/user.ts
@@ -0,0 +1,15 @@
+
+export class Users {
+
+  id : number = 0;
+  firstName : String = "";
+  lastName : String = "";
+  email : String;
+  birthday : Date;
+  login : String;
+  password : String;
+  phone : String;
+  createAt : Date;
+  lastLogin : Date;
+
+}
